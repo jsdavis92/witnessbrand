4a490b48855508fa9d53c6cf6252ad5a70e06bee
Joshua Davis <joshuadavis@joshuas-mbp.mynetworksettings.com>
2024-10-30 23:34:36 -0500

    first witness brand website committ

---
+++ index.js
@@ -0,0 +1,5 @@
+// Show modal on page load
+document.addEventListener("DOMContentLoaded", function() {
+    var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+    welcomeModal.show();
+  });
