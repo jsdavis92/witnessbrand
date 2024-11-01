d3998db94a5076a520591aa944671757930955c5
Joshua Davis <joshuadavis@joshuas-mbp.mynetworksettings.com>
2024-11-01 08:42:39 -0500

    truck product page update

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
     welcomeModal.show();
   });
+
+
+
+  function changeImage(imageUrl) {
+    document.getElementById('mainImage').src = imageUrl;
+
+    // Highlight the active thumbnail
+    document.querySelectorAll('.thumbnail').forEach(el => el.classList.remove('active'));
+    document.querySelector(`img[src="${imageUrl}"]`).classList.add('active');
+}
